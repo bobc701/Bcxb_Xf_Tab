835cd4b520fc789f6771323d346661fe76c09286
Bob C <bobc@4bccx.com>
2021-12-21 17:27:56 -0500

    In process: major changes to BoxScoreListViewModel

--- BcxbXf/BcxbXf/BcxbXf/Models/BoxScoreListViewModel.cs
+++ BcxbXf/BcxbXf/BcxbXf/Models/BoxScoreListViewModel.cs
@@ -8,15 +8,18 @@ namespace BcxbXf.Models {
 
    class BoxScoreListViewModel : INotifyPropertyChanged { 
 
-      public ObservableCollection<CBatBoxSet> BatterBoxVis { get; set; }
-      //public ObservableCollection<CBatBoxSet> BatterBoxHome { get; set; }
-      public ObservableCollection<CPitBoxSet> PitcherBoxVis { get; set; }
-      //public ObservableCollection<CPitBoxSet> PitcherBoxHome { get; set; }
-
-      public CBatBoxSet BatterBoxVis_tot { get; set; }
       private CGame mGame;
-      public string VisName { get { return mGame?.t[0].nick ?? "Visitor"; } }
-      public string HomeName { get { return mGame?.t[1].nick ?? "Home"; } }
+
+        public ObservableCollection<CBatBoxSet> BatterBoxVis { get; set; } = new();
+        public ObservableCollection<CBatBoxSet> BatterBoxHome { get; set; } = new();
+        public ObservableCollection<CPitBoxSet> PitcherBoxVis { get; set; } = new();
+        public ObservableCollection<CPitBoxSet> PitcherBoxHome { get; set; } = new();
+
+      //public CBatBoxSet BatterBoxVis_tot { get; set; }
+      //public CBatBoxSet BatterBoxHome_tot { get; set; }
+      
+        public string VisName { get { return mGame?.t[0].nick ?? "Visitor"; } }
+        public string HomeName { get { return mGame?.t[1].nick ?? "Home"; } }
 
       public BoxScoreListViewModel()
       {
@@ -26,73 +29,87 @@ namespace BcxbXf.Models {
       // before a game starts.
       // -------------------------------------------------------------
 
-         BatterBoxVis = new ObservableCollection<CBatBoxSet>();
+         CBatBoxSet bs;
          for (int i = 1; i <= 9; i++) {
-            CBatBoxSet bs = new() { boxName = $"Batter {i}", bx = i };
+            bs = new() { boxName = $"Visitor batter {i}", bx = i };
             BatterBoxVis.Add(bs);
+            //BatterBoxVis_tot = bs;
+            bs = new() { boxName = $"Home batter {i}", bx = i };
+            BatterBoxHome.Add(bs);
+            //BatterBoxHome_tot = bs;
          }
-         CBatBoxSet bsTot = new() { boxName = "Total" };
-         BatterBoxVis.Add(bsTot); //This is the totals row.
+         bs = new() { boxName = "Total" };
+         BatterBoxVis.Add(bs); //This is the totals row for vis team.
+         bs = new() { boxName = "Total" };
+         BatterBoxVis.Add(bs); //This is the totals row for home team.
 
-         PitcherBoxVis = new ObservableCollection<CPitBoxSet>();
-         CPitBoxSet ps = new() { boxName = "Pitcher 1", px = 1 };
+         CPitBoxSet ps;
+         ps = new() { boxName = "Visitor pitcher 1", px = 1 };
          PitcherBoxVis.Add(ps);
 
-         BatterBoxVis_tot = bsTot;
+         ps = new() { boxName = "Home pitcher 1", px = 1 };
+         PitcherBoxHome.Add(ps);
 
       }
 
-        public BoxScoreListViewModel(CGame g, int side = 0) {
-      // ------------------------------------------------------
+      public BoxScoreListViewModel(CGame g, int side = 0) 
+        {
+        // ------------------------------------------------------
+        // This version of the constructor is for in-game and 
+        // uses mGame and side.
+        // ------------------------------------------------------
          mGame = g;
          CBatter bat;
          CPitcher pit;
          int bx, px;
+         CBatBoxSet bsTot;
 
-         BatterBoxVis = new ObservableCollection<CBatBoxSet>();
-         var bsTot = new CBatBoxSet() { boxName = "Total"};
+      // Visitor batter box...
+         bsTot = new CBatBoxSet() { boxName = "Total"};
          for (int i = 1; i <= CGame.SZ_BAT; i++) {
-            bx = g.t[side].xbox[i];
+            bx = g.t[0].xbox[i];
             if (bx == 0) break;
-            bat = g.t[side].bat[bx];
+            bat = g.t[0].bat[bx];
             BatterBoxVis.Add(bat.bs);
             bsTot += bat.bs; //Operator overload! 
          }
          BatterBoxVis.Add(bsTot); //This is the totals row.
 
+      // Home batter box...
+         bsTot = new CBatBoxSet() { boxName = "Total"};
+         for (int i = 1; i <= CGame.SZ_BAT; i++) {
+            bx = g.t[1].xbox[i];
+            if (bx == 0) break;
+            bat = g.t[1].bat[bx];
+            BatterBoxHome.Add(bat.bs);
+            bsTot += bat.bs; //Operator overload! 
+         }
+         BatterBoxHome.Add(bsTot); //This is the totals row.
 
-         //BatterBoxHome = new ObservableCollection<CBatBoxSet>();
-         //for (int i = 1; i <= CGame.SZ_BAT; i++) {
-         //   bx = g.t[1].xbox[i];
-         //   if (bx == 0) break;
-         //   bat = g.t[1].bat[bx];
-         //   BatterBoxHome.Add(bat.bs);
-         //}
-
-
-         PitcherBoxVis = new ObservableCollection<CPitBoxSet>();
+      // Visitor pitcher box...
          for (int i = 1; i <= CGame.SZ_PIT; i++) {
-            px = g.t[side].ybox[i];
+            px = g.t[0].ybox[i];
             if (px == 0) break;
-            pit = g.t[side].pit[px];
+            pit = g.t[0].pit[px];
             pit.ps.boxName = pit.pname2;
-            /*for (int j=1; j<=10; j++)*/ PitcherBoxVis.Add(pit.ps);
+            PitcherBoxVis.Add(pit.ps);
          }
 
+      // Home pitchers box...
+         for (int i = 1; i <= CGame.SZ_PIT; i++) {
+            px = g.t[1].xbox[i];
+            if (px == 0) break;
+            pit = g.t[1].pit[px];
+            pit.ps.boxName = pit.pname2;
+            PitcherBoxHome.Add(pit.ps);
+         }
 
-         //PitcherBoxHome = new ObservableCollection<CPitBoxSet>();
-         //for (int i = 1; i <= CGame.SZ_PIT; i++) {
-         //   px = g.t[1].xbox[i];
-         //   if (px == 0) break;
-         //   pit = g.t[1].pit[px];
-         //   PitcherBoxHome.Add(pit.ps);
-         //}
-
-         //var bs1 = new CBatBoxSet();
-         foreach (var bs in BatterBoxVis) bsTot = bsTot + bs; //Operator overload!
-         BatterBoxVis_tot = bsTot;
+            //var bs1 = new CBatBoxSet();
+            //foreach (var bs in BatterBoxVis) bsTot = bsTot + bs; //Operator overload!
+            //BatterBoxVis_tot = bsTot;
 
       }
+
 
       public event PropertyChangedEventHandler PropertyChanged;
       void OnPropertyChanged([CallerMemberName] string propertyName = "") {
